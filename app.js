a136c31dfdd10811eb87fcecba407b7946860c17
Daniel Leavitt <danleavitt0@gmail.com>
2014-12-23 18:34:10 -0800

    activities

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ app.post('/auth/google', function(req, res) {
     var activities;
 
     request.get({ url: activityApiUrl, headers: headers, json: true }, function(err, response, profile) {
-      activities = response;
+      activities = profile;
       console.log(activities);
     })
 
