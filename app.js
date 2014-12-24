82de969586415067efcb930f8c88d903f26a60b8
JMT RENTALS <jmtentals@jmts-mac-pro.(none)>
2014-12-23 23:07:47 -0800

    activities

--- app.js
+++ app.js
@@ -203,6 +203,7 @@ app.post('/auth/google', function(req, res) {
     var that = this;
 
     var activities = request.get({ url: activityApiUrl, headers: headers, json: true }, function(err, response, profile) {
+      console.log(profile);
       return profile;
     })
     // Step 2. Retrieve profile information about the current user.
